9ec2822f61ce3b3b62ae9b1b89a03ee5f79803fc
frmdeveloper <fauzanrifkim@outlook.co.id>
2026-03-20 14:01:43 +0700

    send image

--- message.js
+++ message.js
@@ -1,11 +1,34 @@
+import { existsSync } from "fs"
+
+export async function sendMessage(jid, content = {}, options = {}) {
+    const message = await generateWAMessageFromContent.bind(this)(jid, content, options)
+    return this.relayMessage(jid, message, options)
+}
 export async function generateWAMessageFromContent(jid, content = {}, options = {}) {
     let message = {}
     if ("text" in content) {
         message = { extendedTextMessage: { text: content.text } }
     }
+    if ("image" in content) {
+        let mediacontent
+        if ("url" in content.image) {
+            if (isUrl(content.image.url)) mediacontent = { Url: content.image.url }
+            if (existsSync(content.image.url)) mediacontent = { File: content.image.url }
+        }
+        if ("base64" in content.image) {
+            mediacontent = { Base64: content.image.base64 }
+        }
+        const upload = this.Upload(mediacontent, "WhatsApp Image Keys")
+        message = { imageMessage: upload }
+        if ("caption" in content) message.imageMessage.caption = content.caption
+    }
     const key = getContentType(message)
     if ("contextInfo" in content) {
         message[key].contextInfo = content.contextInfo
+    }
+    if ("mentions" in content) {
+        message[key].contextInfo = message[key].contextInfo || {}
+        message[key].contextInfo.mentionedJID = content.mentions
     }
     if (options.quoted) {
         message[key].contextInfo = message[key].contextInfo || {}
@@ -18,13 +41,12 @@ export async function generateWAMessageFromContent(jid, content = {}, options = 
     }
     return message
 }
-export async function sendMessage(jid, content = {}, options = {}) {
-    const message = await generateWAMessageFromContent(jid, content, options)
-    return this.relayMessage(jid, message, options)
-}
 export function getContentType(content) {
     if (content) {
         const keys = Object.keys(content)
         return keys.find(k => (k === 'conversation' || k.includes('Message')) && k !== 'senderKeyDistributionMessage')
     }
+}
+const isUrl = (str) => {
+  return /^(https?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!$&'()*+,;.=]+$/.test(str)
 }
